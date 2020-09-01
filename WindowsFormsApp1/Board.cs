5aa7108867b765bd21cdb1e4a2c3b8321fd7719b
VirgoFrank <virgo.frank@tptlive.ee>
2020-09-01 14:50:22 +0300

    add repo

---
+++ WindowsFormsApp1/Board.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Board
+    {
+        public static int[,] board =
+         {
+                { 7, 8, 0, 4, 0, 0, 1, 2 ,0},
+                { 6, 0, 0, 0, 7, 5, 0, 0, 9},
+                { 0, 0, 0, 6, 0, 1, 0, 7, 8},
+                { 0, 0, 7, 0, 4, 0, 2, 6, 0},
+                { 0, 0, 1, 0, 5, 0, 9, 3, 0},
+                { 9, 0, 4, 0, 6, 0, 0, 0, 5},
+                { 0, 7, 0, 3, 0, 0, 0, 1, 2},
+                { 1, 2, 0, 0, 0, 7, 4, 0, 0},
+                { 0, 4, 9, 2, 0, 6, 0, 0, 7}
+        };
+
+        public bool Solve()
+        {
+            int[] empty = FindEmpty();
+            if (empty == null)
+                return true;
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (check(empty[1], empty[0], i))
+                {
+                    board[empty[0], empty[1]] = i;
+                    if (Solve())
+                        return true;
+                    board[empty[0], empty[1]] = 0;
+                }
+
+            }
+            return false;
+
+        }
+
+        int[] FindEmpty()
+        {
+            int[] EmptySpaceLocation = new int[2];
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == 0)
+                    {
+                        EmptySpaceLocation[0] = i;
+                        EmptySpaceLocation[1] = j;
+
+                        return EmptySpaceLocation;
+                    }
+
+                }
+            }
+            return null;
+        }
+
+        public string[] ComboBoxData(int col, int row)
+        {
+            string[] data = new string[9];
+            int counter = 0;
+            for (int i = 1; i < 10; i++)
+            {
+                if (check(col, row, i))
+                {
+                    data[counter] = i.ToString();
+                    counter++;
+                }
+
+            }
+            string[] data2 = new string[counter];
+            for (int i = 0; i < data.Length; i++)
+            {
+                if(data[i] != null)
+                data2[i] = data[i];
+            }
+            return data2;
+
+        }
+
+        public bool check(int col, int row, int num)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                if (board[row, i] == num)
+                    return false;
+            }
+            for (int i = 0; i < board.GetLength(1); i++)
+            {
+                if (board[i, col] == num)
+                    return false;
+            }
+
+            for (int i = row / 3 * 3; i < row / 3 * 3 + 3; i++)
+            {
+                for (int j = col / 3 * 3; j < col / 3 * 3 + 3; j++)
+                {
+                    if (board[i, j] == num)
+                        return false;
+                }
+
+            }
+            return true;
+
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Board Board = new Board();
+        List<List<ComboBox>> comboBoxes2DList = new List<List<ComboBox>>();
+        private OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Board.Solve();
+            DrawSolvedBoard();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "Solve";
+            button2.Text = "Select .SDK file";
+            Draw();
+
+        }
+        private void DrawSolvedBoard()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    comboBoxes2DList[i][j].Items.Clear();
+                    comboBoxes2DList[i][j].Items.Add(Board.board[i, j]);
+                    comboBoxes2DList[i][j].Text = Board.board[i, j].ToString();
+
+                }
+
+            }
+        }
+
+        private void Draw()
+        {
+
+            for (int i = 0; i < 9; i++)
+            {
+                List<ComboBox> comboBoxes = new List<ComboBox>();
+                comboBoxes2DList.Add(comboBoxes);
+                for (int j = 0; j < 9; j++)
+                {
+                    ComboBox comboBox = new ComboBox();
+                    comboBox.Location = new Point(j * 30, i * 30);
+                    if (Board.board[i, j] == 0)
+                    {
+                        string[] data = Board.ComboBoxData(i, j);
+                        comboBox.Items.AddRange(data);
+                    }
+                    else
+                        comboBox.Items.Add(Board.board[i, j]);
+                    comboBox.Width = 30;
+                    comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+                    comboBox.Text = Board.board[i, j].ToString();
+                    comboBoxes2DList[i].Add(comboBox);
+                    Controls.Add(comboBox);
+                }
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string address = openFileDialog1.FileName;
+
+                string[] Lines = File.ReadAllLines(address);
+                string[] Puzzle = new string[9];
+                int i = 0;
+             
+                while (i < Lines.Length)
+                {
+                    if (Lines[i] == "[Puzzle]")
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            richTextBox1.Text += Lines[i + j + 1].Replace('.', '0') + "\n";
+                            Puzzle[j] = (Lines[i + j + 1].Replace('.', '0'));
+                           
+                        }
+                        break;
+                    }
+                    i++;
+                }
+                for (int a = 0; a < 9; a++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        Board.board[a, j] = Convert.ToInt32( Puzzle[a][j]-48);
+                    }
+                }
+                DrawSolvedBoard();
+            }
+
+        }
+    }
+}
